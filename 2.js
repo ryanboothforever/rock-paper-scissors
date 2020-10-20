ed3e5f813491c3f333f3ba6cb00a71d0e4d44b9b
Ryan Booth <ryanboothforever@gmail.com>
2020-10-20 06:57:55 -0500

    Created a switch statement for gameplay.

--- 2.js
+++ 2.js
@@ -1,1 +1,28 @@
-// TODO
+//created switch statement with game options
+
+const gameRules = choice => {
+    const lowChoice = choice.toLowerCase();
+    let beatsIt;
+switch(lowChoice){
+    case 'scissors':
+        beatsIt = "rock";
+         break;
+    case 'paper': 
+        beatsIt = "scissors";
+        break;
+    case 'rock':
+        beatsIt = "paper";
+        break;
+    default: 
+        beatsIt = "tie";
+
+}
+
+return beatsIt;
+};
+
+/* I feel this statement is easiest to read. However, I don't know 
+how to get my return statement to "add" to the counter 
+(my C and P arrays). Or should I be doing that with a different
+function? */
+
